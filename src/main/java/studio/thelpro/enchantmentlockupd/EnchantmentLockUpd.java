c78f897e6af897da9ec4481276dc9a76aaabd565
Leon <leon.potocki@gmail.com>
2024-05-19 08:47:12 +0200

    Add files via upload

---
+++ src/main/java/studio/thelpro/enchantmentlockupd/EnchantmentLockUpd.java
@@ -0,0 +1,72 @@
+package studio.thelpro.enchantmentlockupd;
+
+import studio.thelpro.enchantmentlockupd.feature.BlockAnvil;
+import studio.thelpro.enchantmentlockupd.feature.BlockEnchantingTable;
+import studio.thelpro.enchantmentlockupd.feature.BlockGrindstone;
+import studio.thelpro.enchantmentlockupd.feature.BlockSmithing;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+/**
+ * Updated to support minecraft 1.20.1.
+ * @author Newt
+ */
+public class EnchantmentLockUpd extends JavaPlugin {
+
+    public boolean block_enchanting_table;
+    public boolean block_anvil_enchanting;
+    public boolean block_anvil_repair;
+    public boolean block_grindstone;
+    public boolean block_smithing;
+    public ItemManager itemManager;
+    public MessageManager messageManager;
+
+    public static EnchantmentLockUpd plugin;
+
+    /**
+     * Enabling the plugin.
+     */
+    public void onEnable() {
+        plugin = this;
+        saveDefaultConfig();
+
+        FileConfiguration configuration = getConfig();
+        if (configuration.getInt("config_version") != 1) {
+            Logger logger = getLogger();
+            logger.severe("Invalid config.yml detected! (Is it outdated?)");
+            logger.severe("Please delete the current file and restart the server.");
+        }
+
+        block_enchanting_table = configuration.getBoolean("block_enchanting_table");
+        block_anvil_enchanting = configuration.getBoolean("block_anvil_enchanting");
+        block_anvil_repair = configuration.getBoolean("block_anvil_repair");
+        block_grindstone = configuration.getBoolean("block_grindstone");
+        block_smithing = configuration.getBoolean("block_smithing");
+
+        itemManager = new ItemManager(this);
+        messageManager = new MessageManager(this);
+        registerEvents();
+    }
+
+    /**
+     * Registers all (enabled) events.
+     */
+    private void registerEvents() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        // Prevent players from enchanting locked items with an Enchanting Table.
+        if (block_enchanting_table) pluginManager.registerEvents(new BlockEnchantingTable(this), this);
+
+        // Prevent players from enchanting and/or repairing locked items with an Anvil.
+        if (block_anvil_enchanting || block_anvil_repair) pluginManager.registerEvents(new BlockAnvil(this), this);
+
+        // Prevent players from disenchanting locked items with a Grindstone.
+        if (block_grindstone) pluginManager.registerEvents(new BlockGrindstone(this), this);
+
+        // Prevent players from upgrading locked items to netherite with a smithing table.
+        if (block_smithing) pluginManager.registerEvents(new BlockSmithing(this), this);
+    }
+}
